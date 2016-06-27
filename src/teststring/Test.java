c1a1f177e05169e57b7f5f4018607880b3c33294
test <yang13994427986@sina.com>
2016-06-27 14:08:30 +0800

    第二次提交

--- src/teststring/Test.java
+++ src/teststring/Test.java
@@ -74,9 +74,7 @@ public class Test {
 	  }
 	  
     public static void main(String args[]){//测试敏感词过滤             
-    	String word = "太多BB弹的伤单红宇感情怀也许只基地针孔摄像头 荧幕中的情节，主人公尝试着去用某种方式渐渐的很潇洒地释自杀怀那些自己经历的伤感。"
-				+ "然后法轮功而卖血过于牵强的把自己的情感也附加于银幕情节中，然后感动就流泪，"
-				+ "难过就躺在某一个人的怀里尽情的阐述心扉或者手机卡黑  火 药的配方复制器一个人部电影在夜三级片 深人静的晚上，关";
+    	String word = "";
         FilteredResult res=WordFilterUtil.filterHtml(word, '*'); 
         System.out.println("优先级："+res.getLevel());//检测到的敏感词中最高优先级的值 0为最小  
         System.out.println("过滤后的："+res.getFilteredContent().toString());//过滤后的字符串  
