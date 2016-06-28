a6b1c7676b876e11fbeb6862b2efb3c816480112
test <yang13994427986@sina.com>
2016-06-28 11:06:41 +0800

    新增4

---
+++ src/main/java/org/smart4j/sample_1/serviceImp/EmployeeServiceImp.java
@@ -0,0 +1,9 @@
+package org.smart4j.sample_1.serviceImp;
+
+import org.smart4j.sample_1.service.EmployeeService;
+
+public class EmployeeServiceImp implements EmployeeService {
+	
+	public void
+
+}
