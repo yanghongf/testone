9e4ff8467b00b9041c10aac52bd68522ee4e29b6
test <yang13994427986@sina.com>
2016-06-28 09:57:46 +0800

    新增2

---
+++ src/main/java/org/smart4j/sample_1/entity/Employee.java
@@ -0,0 +1,42 @@
+package org.smart4j.sample_1.entity;
+
+public class Employee {
+	
+	private int id;
+	private String eName;
+	private int age;
+	private String gender;
+	private String phoneNumber;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String geteName() {
+		return eName;
+	}
+	public void seteName(String eName) {
+		this.eName = eName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+		
+
+}
